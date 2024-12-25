358be363cb861e5b9ee5f0f215e3af70f694bb9b
Jonas Savickas <jonsavickas@gmail.com>
2024-12-25 13:02:03 +0200

    cooking2

--- books.py
+++ books.py
@@ -1,6 +1,8 @@
 import requests
 from dotenv import load_dotenv
 import os
+import csv
+import time
 
 load_dotenv()
 api_key = os.getenv("my_key")
@@ -17,21 +19,31 @@ class Book:
         return f"Title: {self.title}, Author: {self.author}, Rating: {self.rating}, Genre: {self.genre}"
 
 
-def check_book_file(): ...
+def check_book_file():
+    try:
+        with open("collection.csv", "r") as f:
+            reader = csv.reader(f)
+            for row in reader:
+                if row:
+                    return True
+        return "\nCurrently there are no books in a file.\n"
+    except FileNotFoundError:
+        return "\nCurrently there are no books in a file.\n"
 
 
 class Book_Manager:
     def __init__(self):
         pass
 
-    def view_books():
-        pass
+    def view_books(self):
+        check = check_book_file()
+        print(f"{check}")
 
-    def add_books():
-        pass
+    def add_books(self): ...
 
-    def remove_books():
-        pass
+    def remove_books(self):
+        check = check_book_file()
+        print(check)
 
 
 class Book_Recommendation:
--- main.py
+++ main.py
@@ -23,25 +23,25 @@ def main():
         try:
             print("\n1. View books from your collection.")
             print("\n2. Add books to your collection.")
-            print("\n3. Edit books in your collection.")
-            print("\n4. Remove books from your collection.")
-            print("\n5. Generate a book recommendation.")
-            print("\n6. Exit the program.")
+            print("\n3. Remove books from your collection.")
+            print("\n4. Generate a book recommendation.")
+            print("\n5. Exit the program.")
             selection = int(
                 input("\nEnter a number of an option of what you would like to do: ")
             )
             clear_screen()
             if selection == 1:
-                ...
+                view = Book_Manager()
+                view.view_books()
             elif selection == 2:
-                ...
+                add = Book_Manager()
+                add.add_books()
             elif selection == 3:
-                ...
+                remove = Book_Manager()
+                remove.remove_books()
             elif selection == 4:
                 ...
             elif selection == 5:
-                ...
-            elif selection == 6:
                 print(
                     "\nExiting the program! Thanks for using book recommendation and management tool!"
                 )
